f002cad0fd7895dfcd3d04afe2dfe45b5a4a03ed
inadeqtfuturs <alexj.christie@gmail.com>
2025-01-02 09:45:04 -0600

    fix(sitemap): url update

--- apps/web/app/sitemap.ts
+++ apps/web/app/sitemap.ts
@@ -2,19 +2,20 @@ import { getBlogPages } from '@content';
 import type { MetadataRoute } from 'next';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const domain = 'https://alexchristie.dev';
   const blogPages = await getBlogPages({
     filter: ({ frontmatter: { draft } }) => !draft,
   });
 
   const blogPageEntries = blogPages.map(({ params: { slug } }) => ({
-    url: `${process.env.__NEXT_PRIVATE_ORIGIN}/garden/${slug.join('/')}`,
+    url: `${domain}/garden/${slug.join('/')}`,
     lastModified: new Date(),
     changeFrequency: 'weekly',
     priority: 0.7,
   })) as MetadataRoute.Sitemap;
 
   const staticPages = ['', 'garden', 'about'].map((x) => ({
-    url: `${process.env.__NEXT_PRIVATE_ORIGIN}/garden/${x}`,
+    url: `${domain}/${x}`,
     lastModified: new Date(),
     changeFrequency: 'weekly',
     priority: 0.7,
